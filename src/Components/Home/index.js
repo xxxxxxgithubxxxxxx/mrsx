d459b5e8d42a6f04abf0146f70fbd6e3bc9f1ae4
xin <123@qq.com>
2018-05-02 16:22:31 +0800

    test

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,22 +1,64 @@
 import React,{ Component } from "react"
 import "./index.css"
 import logo from "./20180428230526.png"
+//import ReactSwiper from 'react-swiper'
+import axios from "axios"
+
 class Home extends Component {
+	constructor(props){
+        super(props);
+        this.state={
+            datalist:[],
+        }
+    }
+	componentWillMount(){
+        console.log(this.state.datalist.length)
+        axios.get('/show/page/block?pageType=5').then(res=>{
+            
+            if(res.data.data){
+                this.setState({
+                    datalist:res.data.data
+                })
+            }
+        })
+       
+    }
 	render(){
 		return (
 			<div id="home">
-				{
-					<div>
-						<div id="left">
-							<img src={logo} />
-						</div>
-						<div id="right">
-							登录
-						</div>
+				<nav>
+					<div id="left">
+						<img src={logo}/>
 					</div>
-				}
+					<div id="right">
+						<span>登录</span>
+					</div>
+				</nav>
 				
-				home
+			{	
+//				<ReactSwiper className="carousel" swipeOptions={{continuous:true,auto:3000}} key={this.state.datalist.length}>
+//               {
+//                   this.state.datalist.map((item)=>
+//                       <div key={item.id}>
+//                            <img src={"oss.static.nubia.cn/" + item.small_image
+//} style={{width:'100%'}} />
+//                       </div>
+//                   )
+//               }
+//             </ReactSwiper>
+            }
+               
+               
+				
+				<div>
+				</div>
+				
+				<div>
+				</div>
+				
+				<div>
+				</div>
+				
 			</div>
 		)
 	}
